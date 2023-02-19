ae35c4d602fcda4dc4b88ff147679049669bfbcb
Klinovitsky <klinovitsky@gmail.com>
2023-02-19 11:37:58 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Домашнее задание к занятию 1. Знакомство с Python
+# pyfree-homeworks/1.md at main · netology-code/pyfree-homeworks (github.com)
+# Напишите программу, которая последовательно запрашивает у пользователя Дату и Описание задачи, а затем выводит их через пробел.
+
+data_from_user = input("Введите дату: ")
+task_from_user = input("Введите задачу: ")
+print(data_from_user, task_from_user)
+
